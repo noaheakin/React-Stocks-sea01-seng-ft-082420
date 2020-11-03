ad9f36f2a7fcf07d601660923eaf80414119d266
candacecodes <candacehchoe@gmail.com>
2020-11-03 10:50:12 -0800

    sort alphabetically and price work but render is one click behind

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = () => {
-  return (
-    <div>
+const SearchBar = ({ changeSort }) => {
+	return (
+		<div>
+			<strong>Sort by:</strong>
+			<label>
+				<input
+					type="radio"
+					value="Alphabetically"
+					checked={null}
+					onChange={changeSort}
+				/>
+				Alphabetically
+			</label>
+			<label>
+				<input
+					type="radio"
+					value="Price"
+					checked={null}
+					onChange={changeSort}
+				/>
+				Price
+			</label>
+			<br />
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
-
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
-
-
-    </div>
-  );
-}
-
+			<label>
+				<strong>Filter:</strong>
+				<select onChange={null}>
+					<option value="Tech">Tech</option>
+					<option value="Sportswear">Sportswear</option>
+					<option value="Finance">Finance</option>
+				</select>
+			</label>
+		</div>
+	);
+};
 
 export default SearchBar;
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -1,66 +1,82 @@
-import React, { Component } from 'react';
-import StockContainer from './StockContainer'
-import PortfolioContainer from './PortfolioContainer'
-import SearchBar from '../components/SearchBar'
+import React, { Component } from "react";
+import StockContainer from "./StockContainer";
+import PortfolioContainer from "./PortfolioContainer";
+import SearchBar from "../components/SearchBar";
 
 class MainContainer extends Component {
+	state = {
+		stocks: [],
+		myStocks: [],
+		sortSelection: "null",
+	};
 
-  state = {
-    stocks: [],
-    myStocks: []
-  }
+	componentDidMount() {
+		fetch("http://localhost:3000/stocks")
+			.then((res) => res.json())
+			.then((stocks) => this.setState({ stocks }));
+	}
 
-  componentDidMount () {
-    fetch('http://localhost:3000/stocks')
-    .then(res => res.json())
-    .then(stocks => this.setState({stocks}))
-  }
+	// handleClick = (stock) => {
+	//   if (!this.state.myStocks.includes(stock)) {
+	//     this.setState({
+	//       myStocks: [...this.state.myStocks, stock]
+	//     })}
+	//   else {this.setState({
+	//     myStocks: [...this.state.myStocks.filter(s => s !== stock)]
+	//   })}
+	// }
 
-  // handleClick = (stock) => {
-  //   if (!this.state.myStocks.includes(stock)) {
-  //     this.setState({
-  //       myStocks: [...this.state.myStocks, stock]
-  //     })}
-  //   else {this.setState({
-  //     myStocks: [...this.state.myStocks.filter(s => s !== stock)]
-  //   })}
-  // }
+	addStock = (stock) => {
+		if (!this.state.myStocks.includes(stock)) {
+			this.setState({
+				myStocks: [...this.state.myStocks, stock],
+			});
+		}
+	};
 
-  addStock = (stock) => {
-    if (!this.state.myStocks.includes(stock)) {
-      this.setState({
-        myStocks: [...this.state.myStocks, stock]
-      })
-    }
-  }
+	sellStock = (stock) => {
+		this.setState({
+			myStocks: [...this.state.myStocks.filter((s) => s !== stock)],
+		});
+	};
 
-  sellStock = (stock) => {
-    this.setState({
-      myStocks: [...this.state.myStocks.filter(s => s !== stock)]
-    })
-  }
+	changeSort = (e) => {
+		this.setState({
+			sortSelection: e.target.value,
+		});
+		this.sortStocks();
+	};
 
-  render() {
-    return (
-      <div>
-        <SearchBar/>
+	sortStocks = () => {
+		if (this.state.sortSelection === "Alphabetically") {
+			return this.state.stocks.sort((a, b) => (a.name > b.name ? 1 : -1));
+		} else if (this.state.sortSelection === "Price") {
+			return this.state.stocks.sort((a, b) => (a.price > b.price ? 1 : -1));
+		}
+	};
 
-          <div className="row">
-            <div className="col-8">
+	render() {
+		return (
+			<div>
+				<SearchBar changeSort={this.changeSort} />
 
-              <StockContainer stocks={this.state.stocks} addStock={this.addStock}/>
-
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer myStocks={this.state.myStocks} sellStock={this.sellStock}/>
-
-            </div>
-          </div>
-      </div>
-    );
-  }
-
+				<div className="row">
+					<div className="col-8">
+						<StockContainer
+							stocks={this.state.stocks}
+							addStock={this.addStock}
+						/>
+					</div>
+					<div className="col-4">
+						<PortfolioContainer
+							myStocks={this.state.myStocks}
+							sellStock={this.sellStock}
+						/>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default MainContainer;
